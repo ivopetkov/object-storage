8b6283da42e3cbd05007b590acfb8e87f6d0ef2a
Ivo Petkov <ivo@ivopetkov.com>
2016-09-30 22:01:34 +0300

    Added autoloader.

---
+++ autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Object Storage
+ * https://github.com/ivopetkov/object-storage
+ * Copyright (c) 2016 Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
+$classes = array(
+    'IvoPetkov\ObjectStorage' => 'src/ObjectStorage.php',
+    'IvoPetkov\ObjectStorage\ErrorException' => 'src/ObjectStorage/ErrorException.php',
+    'IvoPetkov\ObjectStorage\ObjectLockedException' => 'src/ObjectStorage/ObjectLockedException.php',
+);
+
+spl_autoload_register(function ($class) use ($classes) {
+    if (isset($classes[$class])) {
+        require __DIR__ . '/' . $classes[$class];
+    }
+}, true);
