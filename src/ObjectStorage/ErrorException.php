2ef08ec3a5ca4adf9a89924c138bb99218958f18
Ivo Petkov <ivo@ivopetkov.com>
2018-08-24 18:45:34 +0300

    Removed unnecessary constructors.

--- src/ObjectStorage/ErrorException.php
+++ src/ObjectStorage/ErrorException.php
@@ -11,31 +11,5 @@ namespace IvoPetkov\ObjectStorage;
 
 class ErrorException extends \ErrorException
 {
-
-    /**
-     * Constructs the exception
-     * @param string $message [optional] <p>
-     * The Exception message to throw.
-     * </p>
-     * @param int $code [optional] <p>
-     * The Exception code.
-     * </p>
-     * @param int $severity [optional] <p>
-     * The severity level of the exception.
-     * </p>
-     * @param string $filename [optional] <p>
-     * The filename where the exception is thrown.
-     * </p>
-     * @param int $lineno [optional] <p>
-     * The line number where the exception is thrown.
-     * </p>
-     * @param Exception $previous [optional] <p>
-     * The previous exception used for the exception chaining.
-     * </p>
-     */
-    public function __construct($message = "", $code = 0, $severity = E_ERROR, $filename = __FILE__, $lineno = __LINE__, Exception $previous = NULL)
-    {
-        parent::__construct($message, $code, $severity, $filename, $lineno, $previous);
-    }
-
+    
 }
--- src/ObjectStorage/ObjectLockedException.php
+++ src/ObjectStorage/ObjectLockedException.php
@@ -11,18 +11,5 @@ namespace IvoPetkov\ObjectStorage;
 
 class ObjectLockedException extends \Exception
 {
-
-    /**
-     * Construct the exception
-     * 
-     * @param string $message [optional] The Exception message to throw.
-     * @param int $code [optional] The Exception code.
-     * @param Exception $previous [optional] The previous exception used for the exception chaining.
-     * </p>
-     */
-    public function __construct($message = "", $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-
+    
 }
